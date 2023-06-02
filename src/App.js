ae93392a239705ca0b7842b61bff45e53d2c1b7c
Giuliano Rosso <rossogiuliano@gmail.com>
2023-06-01 21:34:30 -0300

    More work done, all at once

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div className="App">
-      Under construction!
+    <div className="App min-vh-100 pb-5">
+      <span className="d-flex display-1 justify-content-center align-items-center vh-100">
+        Under construction!
+      </span>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Navbar from './grosso-bootreact/components/Navbar';
+import Footer from './grosso-bootreact/components/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Navbar themeSelect />
     <App />
+    <Footer />
   </React.StrictMode>
 );
