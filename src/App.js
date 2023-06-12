9d559a02a51cd7469caa5fdeeef0a2187c073cc2
Giuliano Rosso <rossogiuliano@gmail.com>
2023-06-12 02:15:06 -0300

    Fixed class -> className in App

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App min-vh-100 pb-5">
       <div className="d-flex flex-column justify-content-center align-items-center vh-100">
-        <i class="bi bi-cone-striped display-1 mb-5"></i>
+        <i className="bi bi-cone-striped display-1 mb-5" />
         <span className="display-1">
           Under construction!
         </span>
