b7f235832e78482010ced8d153c5bccf8f68eef7
Giuliano Rosso <rossogiuliano@gmail.com>
2023-06-12 01:54:25 -0300

    Added "visited" tracking

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import Navbar from './grosso-bootreact/components/Navbar';
 import Footer from './grosso-bootreact/components/Footer';
+import Analytics from './grosso-bootreact/utils/analytics'
+
+Analytics.visited();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
